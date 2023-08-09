42a2cd2b6241846e38cc1ce0107ae5efa5e4c7a8
Romyhoyos <hoyosandrea977>
2023-08-09 20:37:55 -0300

    Prueba Tour

---
+++ indice.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+def main():
+    root = tk.Tk()
+    root.title('Tour Musical')
+    root.iconbitmap('img/musica.ico')
+    root.resizable(0,0)
+
+    root.mainloop()
+
+if _name_=='_main_':
+    main()
