4ef6d2cbaeccdb1c678238a74a517998848391d3
Romyhoyos <hoyosandrea977>
2023-08-10 09:39:17 -0300

    mod

--- indice.py
+++ indice.py
@@ -1,11 +1,11 @@
-from tkinter import tk
+from tkinter import ttk
 from tkinter import *
 from tkinter import tkintermapview
 from tkinter import mapview
 import json
 
 
-class ListaDeEventosApp(tk,Tk):
+class ListaDeEventosApp(ttk,Tk):
     def _init_(self):
         super()._init_()
         self.title("Tour Musical") 
@@ -17,14 +17,14 @@ class ListaDeEventosApp(tk,Tk):
         self.create_widgets()
 
     def create_widgets(self):
-        self.event_listbox = tk.Listbox(self)
-        self.event_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.event_listbox = ttk.Listbox(self)
+        self.event_listbox.pack(side=ttk.LEFT, fill=ttk.BOTH, expand=True)
         self.event_listbox.bind("<<ListboxSelect>>", self.show_event_details)
-        self.event_details_label = tk.Label(self, text="Detalles del evento")
+        self.event_details_label = ttk.Label(self, text="Detalles del evento")
         self.event_details_label.pack()
 
         self.map_view = mapview(self, width=400, height=400)
-        self.map_view.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        self.map_view.pack(side=ttk.RIGHT, fill=ttk.BOTH, expand=True)
 
         self.load_event_list()
 
@@ -33,9 +33,9 @@ class ListaDeEventosApp(tk,Tk):
             return json.load(f)
 
     def load_event_list(self):
-        self.event_listbox.delete(0, tk.END)
+        self.event_listbox.delete(0, ttk.END)
         for event in self.filtered_events:
-            self.event_listbox.insert(tk.END, event["nombre"])
+            self.event_listbox.insert(ttk.END, event["nombre"])
 
     def show_event_details(self, event):
         selected_index = self.event_listbox.curselection()
@@ -44,41 +44,41 @@ class ListaDeEventosApp(tk,Tk):
             self.event_details_label.config(text=f"Nombre: {selected_event['nombre']}\nArtista: {selected_event['artista']}\nGénero: {selected_event['genero']}\nUbicación: {selected_event['ubicacion']}")
             self.map_view.show_location(selected_event["ubicacion"])
 def create_widgets(self):
-        self.event_listbox = tk.Listbox(self)
-        self.event_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.event_listbox = ttk.Listbox(self)
+        self.event_listbox.pack(side=ttk.LEFT, fill=ttk.BOTH, expand=True)
         self.event_listbox.bind("<<ListboxSelect>>", self.show_event_details)
 
-        self.event_details_label = tk.Label(self, text="Detalles del evento")
+        self.event_details_label = ttk.Label(self, text="Detalles del evento")
         self.event_details_label.pack()
 
         self.map_view = mapview(self, width=400, height=400)
-        self.map_view.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        self.map_view.pack(side=ttk.RIGHT, fill=ttk.BOTH, expand=True)
 
         self.load_event_list()
 
-        self.review_button = tk.Button(self, text="Escribir Reseña", command=self.write_review)
+        self.review_button = ttk.Button(self, text="Escribir Reseña", command=self.write_review)
         self.review_button.pack()
 
-        self.history_button = tk.Button(self, text="Historial de Eventos Asistidos", command=self.show_attended_events)
+        self.history_button = ttk.Button(self, text="Historial de Eventos Asistidos", command=self.show_attended_events)
         self.history_button.pack()
 
-        self.mood_label = tk.Label(self, text="Estado de Ánimo del Comentario")
+        self.mood_label = ttk.Label(self, text="Estado de Ánimo del Comentario")
         self.mood_label.pack()
 
-        self.positive_button = tk.Button(self, text="😃", command=lambda: self.set_mood("positivo"))
-        self.positive_button.pack(side=tk.LEFT)
+        self.positive_button = ttk.Button(self, text="😃", command=lambda: self.set_mood("positivo"))
+        self.positive_button.pack(side=ttk.LEFT)
 
-        self.negative_button = tk.Button(self, text="😔", command=lambda: self.set_mood("negativo"))
-        self.negative_button.pack(side=tk.LEFT)
+        self.negative_button = ttk.Button(self, text="😔", command=lambda: self.set_mood("negativo"))
+        self.negative_button.pack(side=ttk.LEFT)
 
 def load_events(self):
         with open("eventos.json") as f:
             return json.load(f)
 
 def load_event_list(self):
-        self.event_listbox.delete(0, tk.END)
+        self.event_listbox.delete(0, ttk.END)
         for event in self.filtered_events:
-            self.event_listbox.insert(tk.END, event["nombre"])
+            self.event_listbox.insert(ttk.END, event["nombre"])
 
 def show_event_details(self, event):
         selected_index = self.event_listbox.curselection()
@@ -91,17 +91,17 @@ def write_review(self):
         selected_index = self.event_listbox.curselection()
         if selected_index:
             selected_event = self.filtered_events[selected_index[0]]
-            review_window = tk.Toplevel(self)
+            review_window = ttk.Toplevel(self)
             review_window.title("Escribir Reseña")
             review_window.geometry("400x300")
 
-            review_label = tk.Label(review_window, text=f"Reseña para {selected_event['nombre']}")
+            review_label = ttk.Label(review_window, text=f"Reseña para {selected_event['nombre']}")
             review_label.pack()
 
-            review_text = tk.Text(review_window)
+            review_text = ttk.Text(review_window)
             review_text.pack()
 
-            submit_button = tk.Button(review_window, text="Enviar", command=lambda: self.submit_review(selected_event, review_text.get("1.0", tk.END)))
+            submit_button = ttk.Button(review_window, text="Enviar", command=lambda: self.submit_review(selected_event, review_text.get("1.0", ttk.END)))
             submit_button.pack()
 
 def submit_review(self, event, review):
